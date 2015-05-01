b78cd59ef73f1985a2912a4765689d244a67de95
Christine Oo <christine@favoritemedium.com>
2015-05-01 17:18:33 +0900

    [clean up] Remove unused code

--- app/controllers/feeds/new.js
+++ app/controllers/feeds/new.js
@@ -17,30 +17,7 @@ export default Ember.ObjectController.extend({
       var model = this.get('model');
       model.deleteRecord();
       this.target.transitionTo('feeds.index');
-    },
-    userLogin: function(){
-      var self = this;
-      //this.getProperties automatically return the key, value hash of the property provided
-      var data = this.getProperties('email','password');
-      console.log("data.email: " +data.email);
-      console.log("data.password: " +data.password);
-
-      Ember.$.ajax({
-        type: "GET",
-        url: "https://stageapp-leprachaun.herokuapp.com/",
-        dataType: 'text/html',
-        success: function(data){
-          if (data.results){
-            self.set('token', data.results.authtoken);
-            self.transitionToRoute('home');
-          }
-          else {
-            self.set('errorMessage', "invalid email/password");
-          }
-        }
-      });
     }
-
   }
 });
 

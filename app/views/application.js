9e572192b537391327ff2c71e535e9af6184e3f3
Christine Oo <christine@favoritemedium.com>
2015-07-23 10:17:31 +0900

    [Iframe height] Set iframe height base on window height

--- app/views/application.js
+++ app/views/application.js
@@ -1,5 +1,10 @@
 import Ember from "ember";
 
 export default Ember.View.extend({
-    classNames: ["ember-app"]
+    classNames: ["ember-app"],
+
+    didInsertElement: function() {
+      document.getElementById('iframeid').style.height = (window.innerHeight-80) + "px"
+    }
+
 });

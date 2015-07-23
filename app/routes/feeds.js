a5017e40478f6e1cbe15a3df3474625084ce6552
Christine Oo <christine@favoritemedium.com>
2015-07-23 10:21:18 +0900

    [Category] Toggle list for each category

--- app/routes/feeds.js
+++ app/routes/feeds.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return this.store.findAll('feed');
+  },
+
+  actions: {
+    showCategory: function(category){
+      if (category == 'android'){
+        this.controller.toggleProperty('isShowAndroidCategory');
+      }
+      else if (category == 'design'){
+        this.controller.toggleProperty('isShowDesignCategory');
+      }
+      else if (category == 'news'){
+        this.controller.toggleProperty('isShowNewsCategory');
+      }
+    }
   }
 });
